629c801e75c441490a3089a1c0aad95a9bd6c81c
KirikaP <kirikahhh@gmail.com>
2024-02-13 17:17:12 +0000

    define action_reward function, sigma and miu_list

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 import numpy as np
-import matplotlib as mpl
 import matplotlib.pyplot as plt
-import random
-import math
 
-""" 我们使用UCB方法 """
+
+sigma = 1  # 10-armed bandit normal distribution std deviation
+# 10-armed bandit normal distribution miu list
+miu_list = [0.2, -0.85, 1.55, 0.3, 1.2, -1.5, -0.2, -1.0, 0.9, -0.6]
+
+# 10-armed bandit
+def action_reward(action):
+    reward = miu_list[action] + np.random.normal(0, scale=sigma)
+    return reward
