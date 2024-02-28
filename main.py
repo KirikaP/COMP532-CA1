16149930d6470579902699e10beb1432d763397d
RyanLiu <isryanyhliu@foxmail.com>
2024-02-28 14:46:06 +0000

    修改注释

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-sigma = 1  # 10-armed bandit normal distribution std deviation
-# 10-armed bandit normal distribution miu list
-miu_list = [0.2, -0.85, 1.55, 0.3, 1.2, -1.5, -0.2, -1.0, 0.9, -0.6]
+sigma = 1  # 正态分布的标准差
+miu_list = [0.2, -0.85, 1.55, 0.3, 1.2, -1.5, -0.2, -1.0, 0.9, -0.6] # 每个臂的奖励期望值
 
 # 10-armed bandit
 def action_reward(action):
     reward = miu_list[action] + np.random.normal(0, scale=sigma)
+    # print("action: ", action, "reward: ", reward)
     return reward
