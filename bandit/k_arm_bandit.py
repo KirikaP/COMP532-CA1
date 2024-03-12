706dc819b75e6c1583df0341fa161ccd10126915
RyanLiu <isryanyhliu@foxmail.com>
2024-03-12 22:08:38 +0000

    lab code

--- bandit/k_arm_bandit.py
+++ bandit/k_arm_bandit.py
@@ -4,7 +4,7 @@ class BanditMachine:
     def __init__(self, n_arms):
         self.n_arms = n_arms  # 赌博机的臂数
         
-        self.sigma = 1  # 所有臂的奖励标准差为1
+        self.sigma = 2  # 所有臂的奖励标准差为1
         self.miu_list = np.random.normal(0, self.sigma, n_arms)  # 初始化每个臂的期望奖励为不同的值，均值为0
         self.counts = np.zeros(n_arms, dtype=int)  # 记录每个臂被拉动的次数
         self.values = np.zeros(n_arms)  # 记录每个臂的估计价值
